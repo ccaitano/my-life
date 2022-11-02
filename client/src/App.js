b55a308b6b30886c66be6fb5b0d81db06a6918c0
B.O.A Addington <Franswarduvar>
2022-11-02 11:12:18 -0700

    the boxes can be moved and the position changed, going to consult group about what we should do next

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
+import Draggable from 'react-draggable';
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
