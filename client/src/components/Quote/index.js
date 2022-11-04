e5623ead60b3b4225546dd58366beaede690bfa8
B.O.A Addington <Franswarduvar>
2022-11-04 12:43:27 -0700

    trial

--- client/src/components/Quote/index.js
+++ client/src/components/Quote/index.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import { useState } from 'react'; 
+import { Button } from './Button.js'; 
+import { ListComponent } from './ListComponent.js'; 
 
-const Quote = () => {
-
-    return (
-        <>
-            <h2>Positive Affirmation/Quote of the Day</h2>
-            {/* Code goes here! */}
-        </>
-    );
-  };
+function Quote() { 
   
-  export default Quote;
+  const [components, setComponents] = useState(["Mercury"]); 
+  const [componentNames, setComponentNames] = useState([ 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune' ]); 
+  
+  function addComponent() { 
+    
+    if (componentNames.length > 0) { 
+      
+      setComponents([...components, componentNames[0]]);
+      componentNames.splice(0, 1);
+      
+    } else { 
+      
+      window.alert("No more planets to add!");
+      
+    } 
+    
+  } 
+  
+  return ( 
+    
+    <div> 
+    
+      <Button onClick={addComponent} text="Call Component"/> 
+      {components.map((item, i) => ( <ListComponent text={item} /> ))} 
+      
+    </div> 
+    
+  ) 
+  
+} 
+
+export default Quote;
