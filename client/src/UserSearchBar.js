a283dcd7fd1c16a4b38d39ae2d75d5c565d68c57
B.O.A Addington <Franswarduvar>
2022-11-07 09:27:33 -0800

    trial 2

--- client/src/UserSearchBar.js
+++ client/src/UserSearchBar.js
@@ -10,21 +10,22 @@ function SearchBar({ placeholder, data }) {
   const handleFilter = (event) => {
     const searchWord = event.target.value;
     setWordEntered(searchWord);
-    const newFilter = data.filter((value) => {
-      return value.title.toLowerCase().includes(searchWord.toLowerCase());
-    });
+    // const newFilter = data.filter((value) => {
+    //   return value.title.toLowerCase().includes(searchWord.toLowerCase());
+    // });
 
     if (searchWord === "") {
       setFilteredData([]);
-    } else {
-      setFilteredData(newFilter);
     }
+    //  else {
+    //   setFilteredData(newFilter);
+    // }
   };
 
-  const clearInput = () => {
-    setFilteredData([]);
-    setWordEntered("");
-  };
+//   const clearInput = () => {
+//     setFilteredData([]);
+//     setWordEntered("");
+//   };
 
   return (
     <div className="search">
@@ -35,13 +36,13 @@ function SearchBar({ placeholder, data }) {
           value={wordEntered}
           onChange={handleFilter}
         />
-        <div className="searchIcon">
-          {/* {filteredData.length === 0 ? (
+        {/* <div className="searchIcon">
+          {filteredData.length === 0 ? (
             <SearchIcon />
           ) : (
             <CloseIcon id="clearBtn" onClick={clearInput} />
-          )} */}
-        </div>
+          )}
+        </div> */}
       </div>
       {filteredData.length !== 0 && (
         <div className="dataResult">
