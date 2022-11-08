e1ace1e7cc25b188776674fa4a81b1d9737b9f41
ccaitano <cheryl.caitano@gmail.com>
2022-11-07 17:08:11 -1000

    Fix Home Page after merges

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -18,73 +18,10 @@ const Home = () => {
 
   return (
     <Container maxWidth="lg" background="none" sx={{ mt: 4, mb: 4 }} >
-
-            <Grid container spacing={4}>
-
-
-            <box>
-                {/* <Draggable> */}
-              {/* Weather API Container */}
-              <Grid item xs={12} md={8} lg={4}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 240,
-                  }}
-                >
-                  <Weather />
-                </Paper>
-              </Grid>
-              {/* </Draggable> */}
-              </box>
-
-                <box>
-                {/* <Draggable> */}
-              {/* To-Do List Container */}
-              <Grid item xs={12} md={4} lg={8}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    height: 800,
-                    maxWidth: '150%'
-                  }}
-                >
-                  <ToDoList />
-                </Paper>
-              </Grid>
-              {/* </Draggable> */}
-              </box>
-
-
-              <box>
-                {/* <Draggable> */}
-              
-              {/* Positive Affirmation Container */}
-              <Grid item xs={12} md={4} lg={8}>
-                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', height: 240, }}>
-                  <Quote />
-                </Paper>
-              </Grid>
-              {/* </Draggable> */}
-              </box>
-
-              <box>
-                {/* <Draggable> */}
-              {/* Chart/History Container */}
-              <Grid item xs={12} md={8} lg={4}>
-                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', height: 240, }}>
-                  <Chart />
-                </Paper>
-              </Grid>
-              {/* </Draggable> */}
-              </box>
-
       <Grid container spacing={4}>
         {/* Weather API Container */}
         <Box>
-          <Draggable>
+          {/* <Draggable> */}
             <Grid item xs={12} md={8} lg={4}>
               <Paper
                 sx={{
@@ -98,11 +35,11 @@ const Home = () => {
               </Paper>
 
             </Grid>
-          </Draggable>
+          {/* </Draggable> */}
         </Box>
         {/* To-Do List Container */}
         <Box>
-          <Draggable>
+          {/* <Draggable> */}
             <Grid item xs={12} md={4} lg={8}>
               <Paper
                 sx={{
@@ -114,11 +51,11 @@ const Home = () => {
                 <ToDoList />
               </Paper>
             </Grid>
-          </Draggable>
+          {/* </Draggable> */}
         </Box>
         {/* Positive Affirmation Container */}
         <Box>
-          <Draggable>
+          {/* <Draggable> */}
             <Grid item xs={12} md={4} lg={8}>
               <Paper 
                 sx={{ 
@@ -131,11 +68,11 @@ const Home = () => {
                 <Quote />
               </Paper>
             </Grid>
-          </Draggable>
+          {/* </Draggable> */}
         </Box>
         {/* Chart/History Container */}
         <Box>
-          <Draggable>   
+          {/* <Draggable>    */}
             <Grid item xs={12} md={8} lg={4}>
               <Paper 
                 sx={{ 
@@ -148,7 +85,7 @@ const Home = () => {
                 <Chart />
               </Paper>
             </Grid>
-          </Draggable>
+          {/* </Draggable> */}
         </Box>
       </Grid>
     </Container>
