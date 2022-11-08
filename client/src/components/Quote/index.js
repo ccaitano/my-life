c1ae131459314bb74597b5c942e3cedf93b47d86
ccaitano <cheryl.caitano@gmail.com>
2022-11-07 22:53:08 -1000

    Update new random positive affirmation on page reload

--- client/src/components/Quote/index.js
+++ client/src/components/Quote/index.js
@@ -2,33 +2,18 @@ import { useState } from 'react';
 import { Button } from './Button.js'; 
 import { ListComponent } from './ListComponent.js'; 
 
-function Quote() { 
+const Quote = () => { 
   
-  const [components, setComponents] = useState(["Hey you got this!"]); 
-  const [componentNames] = useState([ 'Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7' ]); 
+  const affirmations = [ "Hey you got this!", 'Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7' ]; 
+  const randomNumber = Math.floor(Math.random()* affirmations.length);
   
-  function addComponent() { 
-    
-    if (componentNames.length > 0) { 
-      
-      setComponents([...components, componentNames[0]]);
-      componentNames.splice(0, 1);
-      
-    } 
-  } 
-
-    console.log("yoooo whats good"); 
-  return ( 
-    
+  return (  
     <div> 
-    
-      <Button onClick={addComponent} text="Get a new affermation"/> 
-      {components.map((item, i) => ( <ListComponent text={item} /> ))} 
-      
-    </div> 
-    
+      <h1>Positive Affirmation</h1>
+      {/* <Button onClick={addComponent} text="Get a new affermation"/>  */}
+      <p>{affirmations[randomNumber]}</p>
+    </div>    
   ) 
-  
-} 
+};
 
 export default Quote;
