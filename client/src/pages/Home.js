40a31f3e71b5b59293c239c81f31375e1ebcaf30
ryanmerced <ryan_merced@hotmail.com>
2022-11-08 22:04:24 -0800

    hidden home dash grids to create landing page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,6 +6,7 @@ import Weather from '../components/Weather/index';
 import ToDoList from '../components/ToDo/index';
 import Quote from '../components/Quote/index';
 import Chart from '../components/Chart/index';
+import Auth from '../utils/auth';
 
 // import Draggable from 'react-draggable';
 
@@ -18,76 +19,79 @@ const Home = () => {
 
   return (
     <Container maxWidth="lg" background="none" sx={{ mt: 4, mb: 4 }} >
-      <Grid container spacing={4}>
-        {/* Weather API Container */}
-        <Box>
-          {/* <Draggable> */}
-            <Grid item xs={12} md={8} lg={4}>
-              <Paper
-                sx={{
-                  p: 2,
-                  display: 'flex',
-                  flexDirection: 'column',
-                  height: 240,
-                }}
-              >
-                <Weather />
-              </Paper>
+      {Auth.loggedIn() ? (
+                <>
+        <Grid container spacing={4}>
+          {/* Weather API Container */}
+          <Box>
+            {/* <Draggable> */}
+              <Grid item xs={12} md={8} lg={4}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: 240,
+                  }}
+                >
+                  <Weather />
+                </Paper>
 
-            </Grid>
-          {/* </Draggable> */}
-        </Box>
-        {/* To-Do List Container */}
-        <Box>
-          {/* <Draggable> */}
-            <Grid item xs={12} md={4} lg={8}>
-              <Paper
-                sx={{
-                  p: 2,
-                  height: 800,
-                  maxWidth: '150%'
-                }}
-              >
-                <ToDoList />
-              </Paper>
-            </Grid>
-          {/* </Draggable> */}
-        </Box>
-        {/* Positive Affirmation Container */}
-        <Box>
-          {/* <Draggable> */}
-            <Grid item xs={12} md={4} lg={8}>
-              <Paper 
-                sx={{ 
-                  p: 2, 
-                  display: 'flex', 
-                  flexDirection: 'column', 
-                  height: 240, 
-                }}
-              >
-                <Quote />
-              </Paper>
-            </Grid>
-          {/* </Draggable> */}
-        </Box>
-        {/* Chart/History Container */}
-        <Box>
-          {/* <Draggable>    */}
-            <Grid item xs={12} md={8} lg={4}>
-              <Paper 
-                sx={{ 
-                  p: 2, 
-                  display: 'flex', 
-                  flexDirection: 'column', 
-                  height: 240, 
-                }}
-              >
-                <Chart />
-              </Paper>
-            </Grid>
-          {/* </Draggable> */}
-        </Box>
-      </Grid>
+              </Grid>
+            {/* </Draggable> */}
+          </Box>
+          {/* To-Do List Container */}
+          <Box>
+            {/* <Draggable> */}
+              <Grid item xs={12} md={4} lg={8}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    height: 800,
+                    maxWidth: '150%'
+                  }}
+                >
+                  <ToDoList />
+                </Paper>
+              </Grid>
+            {/* </Draggable> */}
+          </Box>
+          {/* Positive Affirmation Container */}
+          <Box>
+            {/* <Draggable> */}
+              <Grid item xs={12} md={4} lg={8}>
+                <Paper 
+                  sx={{ 
+                    p: 2, 
+                    display: 'flex', 
+                    flexDirection: 'column', 
+                    height: 240, 
+                  }}
+                >
+                  <Quote />
+                </Paper>
+              </Grid>
+            {/* </Draggable> */}
+          </Box>
+          {/* Chart/History Container */}
+          <Box>
+            {/* <Draggable>    */}
+              <Grid item xs={12} md={8} lg={4}>
+                <Paper 
+                  sx={{ 
+                    p: 2, 
+                    display: 'flex', 
+                    flexDirection: 'column', 
+                    height: 240, 
+                  }}
+                >
+                  <Chart />
+                </Paper>
+              </Grid>
+            {/* </Draggable> */}
+          </Box>
+        </Grid>
+        </>) : ('')} 
     </Container>
   );
 };
