15dfa5f3b164774875e15d9ddc3c3bd8869f2a09
ccaitano <cheryl.caitano@gmail.com>
2022-11-09 16:31:32 -1000

    Update html to show mylife title and connection file updated mongodb uri

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/my-life',
+  process.env.MONGODB_URI || 'mongodb://localhost/my-life',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
